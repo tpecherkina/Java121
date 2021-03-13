679864e7f9c9d79304d727956bb50155140e4a32
tpecherkina <tpecherkina18@gmail.com>
2021-03-13 16:40:24 -0500

    Init commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int accountbalance = 2_000_000_000;
+        int transfer = 500_000_000;
+        int finalbalance;
+        finalbalance = accountbalance+transfer;
+        System.out.println(finalbalance);
+    }
+
+}
